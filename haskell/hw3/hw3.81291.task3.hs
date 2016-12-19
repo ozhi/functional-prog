6e38926ad30435c263c463dcb8464671d2d3fa61
Bozhin Katsarski <bozhin@gmail.com>
2016-12-20 01:18:43 +0200

    Add 3zad 3) solution

--- haskell/hw3/hw3.81291.task3.hs
+++ haskell/hw3/hw3.81291.task3.hs
@@ -8,6 +8,17 @@ main = do
 	print (compressRLE "aaabbbccc")
 	print (compressRLE "abc")
 	
+	print (getRLE [('m',1),('i',1),('s',2),('i',1), ('s',2),('i',1),('p',2),('i',1)] 0) -- -> ‘m’
+	print (getRLE [('m',1),('i',1),('s',2),('i',1), ('s',2),('i',1),('p',2),('i',1)] 1) -- -> ‘i’
+	print (getRLE [('m',1),('i',1),('s',2),('i',1), ('s',2),('i',1),('p',2),('i',1)] 2) -- -> ‘s’
+	print (getRLE [('m',1),('i',1),('s',2),('i',1), ('s',2),('i',1),('p',2),('i',1)] 3) -- -> ‘s’
+	print (getRLE [('m',1),('i',1),('s',2),('i',1), ('s',2),('i',1),('p',2),('i',1)] 4) -- -> ‘i’
+	print (getRLE [('m',1),('i',1),('s',2),('i',1), ('s',2),('i',1),('p',2),('i',1)] 5) -- -> ‘s’
+	print (getRLE [('m',1),('i',1),('s',2),('i',1), ('s',2),('i',1),('p',2),('i',1)] 6) -- -> ‘s’
+	print (getRLE [('m',1),('i',1),('s',2),('i',1), ('s',2),('i',1),('p',2),('i',1)] 7) -- -> ‘i’
+	print (getRLE [('m',1),('i',1),('s',2),('i',1), ('s',2),('i',1),('p',2),('i',1)] 8) -- -> ‘p’
+	print (getRLE [('m',1),('i',1),('s',2),('i',1), ('s',2),('i',1),('p',2),('i',1)] 9) -- -> ‘p’
+	print (getRLE [('m',1),('i',1),('s',2),('i',1), ('s',2),('i',1),('p',2),('i',1)] 10) -- -> ‘i’
 
 extractRLE :: (Eq a) => [(a, Int)] -> [a]
 extractRLE [] = []
@@ -34,4 +45,10 @@ compressRLE (x:xs) = (x, (countXFromBeginning (x:xs))) : (compressRLE (removeXFr
         if (f == x)
         then (removeXFromBeginning fs)
         else f:fs
+
+getRLE :: (Eq a) => [(a, Int)] -> Int -> a
+getRLE ((sym, times):rest) index =
+    if (index + 1 <= times)
+    then sym
+    else (getRLE rest (index - times))
 
