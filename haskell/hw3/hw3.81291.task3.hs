da15fa243e7e4ff4c0fb8374f5ad3e3bb51ffcb6
Bozhin Katsarski <bozhin@gmail.com>
2016-12-23 11:47:25 +0200

    Add forall to haskell/hw3/task3 and create gitignore

--- haskell/hw3/hw3.81291.task3.hs
+++ haskell/hw3/hw3.81291.task3.hs
@@ -1,26 +1,27 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 main :: IO()
 main = do
-	print (extractRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)])
-	-- -> “mississippi”
-	
-	print (compressRLE "mississippi")
-	print (compressRLE "AAAAAAAAA")
-	print (compressRLE "aaabbbccc")
-	print (compressRLE "abc")
-		
-	print $ compressRLE . extractRLE . compressRLE . extractRLE . compressRLE $ "mississippi"
-	
-	print (getRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)] 0) -- -> ‘m’
-	print (getRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)] 1) -- -> ‘i’
-	print (getRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)] 2) -- -> ‘s’
-	print (getRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)] 3) -- -> ‘s’
-	print (getRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)] 4) -- -> ‘i’
-	print (getRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)] 5) -- -> ‘s’
-	print (getRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)] 6) -- -> ‘s’
-	print (getRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)] 7) -- -> ‘i’
-	print (getRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)] 8) -- -> ‘p’
-	print (getRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)] 9) -- -> ‘p’
-	print (getRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)] 10) -- -> ‘i’
+    print (extractRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)])
+    -- -> “mississippi”
+
+    print (compressRLE "mississippi")
+    print (compressRLE "AAAAAAAAA")
+    print (compressRLE "aaabbbccc")
+    print (compressRLE "abc")
+    print $ compressRLE . extractRLE . compressRLE . extractRLE . compressRLE $ "mississippi"
+
+    print (getRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)] 0) -- -> ‘m’
+    print (getRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)] 1) -- -> ‘i’
+    print (getRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)] 2) -- -> ‘s’
+    print (getRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)] 3) -- -> ‘s’
+    print (getRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)] 4) -- -> ‘i’
+    print (getRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)] 5) -- -> ‘s’
+    print (getRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)] 6) -- -> ‘s’
+    print (getRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)] 7) -- -> ‘i’
+    print (getRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)] 8) -- -> ‘p’
+    print (getRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)] 9) -- -> ‘p’
+    print (getRLE [('m',1),('i',1),('s',2),('i',1),('s',2),('i',1),('p',2),('i',1)] 10) -- -> ‘i’
 
 extractRLE :: (Eq a) => [(a, Int)] -> [a]
 extractRLE [] = []
@@ -30,18 +31,18 @@ extractRLE (listItem:restOfList) = (extractListItem listItem) ++ (extractRLE res
     extractListItem (_, 0) = []
     extractListItem (element, times) = element : (extractListItem (element, times-1))
     
-compressRLE :: (Eq a) => [a] -> [(a, Int)] --forall?
+compressRLE :: forall a. Eq a => [a] -> [(a, Int)]
 compressRLE [] = []
 compressRLE (x:xs) = (x, (countXFromBeginning (x:xs))) : (compressRLE (removeXFromBeginning xs))
     where
-    --countXFromBegcd inning :: [a] -> Int -- why do we have to skip this signature?
+    countXFromBeginning :: [a] -> Int
     countXFromBeginning [] = 0
     countXFromBeginning (f:fs) =
         if f == x
         then 1 + (countXFromBeginning fs)
         else 0
     
-    --removeXFromBeginning :: [a] -> [a]
+    removeXFromBeginning :: [a] -> [a]
     removeXFromBeginning [] = []
     removeXFromBeginning (f:fs) =
         if f == x
