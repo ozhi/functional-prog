d80b39d33d372203ee35961d863a0f7e4f54042c
Bozhin Katsarski <bozhin@gmail.com>
2017-01-21 12:45:18 +0200

    Add more solutions

--- haskell/test2-prep/radorado.hs
+++ haskell/test2-prep/radorado.hs
@@ -62,6 +62,65 @@ repeater content = (\ count glue -> repeat count glue)
         repeat 1 _= content
         repeat count glue = content ++ glue ++ (repeat (count - 1) glue) 
 
+data Order =
+    Online Float Int Int | -- price, order number, delivery (hours)
+    Offline Float -- price
+
+isOnline :: Order -> Bool
+isOnline (Online _ _ _) = True
+isOnline (Offline _) = False
+
+timeiUntilReceiving :: Order -> Int
+timeiUntilReceiving (Online _ _ hours) = hours
+
+totalPrice :: [Order] -> Float
+totalPrice [] = 0
+totalPrice ((Online price _ _) : rest) = price + (totalPrice rest)
+totalPrice ((Offline price) : rest) = price + (totalPrice rest) 
+
+onlineOrders :: [Order] -> Int
+onlineOrders [] = 0
+onlineOrders ((Online _ _ _) : rest) = 1 + (onlineOrders rest)
+onlineOrders ((Offline _) : rest) = (onlineOrders rest) 
+
+isExpensive :: Order -> Bool
+isExpensive (Online price _ _) = price > 100
+isExpensive (Offline price) = price > 100
+
+instance Show Order where
+    show (Online price number delivery) =
+        "Order: [type: online, number: " ++ (show number)
+        ++ ", price: " ++ (show price) ++ ", delivery: " ++ (show delivery) ++ "]"
+
+    show (Offline price) =
+        "Order: [type: offline, price: " ++ (show price) ++ "]"
+
+instance Eq Order where
+    (==) (Offline pr1) (Offline pr2) = pr1 == pr2
+    (==) (Online pr1 _ _) (Online pr2 _ _) = pr1 == pr2
+    (==) _ _ = False
+
+data Tree a = Empty | Node a (Tree a) (Tree a)
+
+levelSum :: Int -> Tree Int -> Int
+levelSum _ Empty = 0
+levelSum 0 (Node root _ _) = root
+levelSum lvl (Node _ left right) = (levelSum (lvl-1) left) + (levelSum (lvl-1) right)
+
+height :: Tree a -> Int
+height Empty = 0
+height (Node _ left right) = 1 + max (height left) (height right)
+
+cone :: Tree Int -> Bool
+cone tree = helper tree (height tree)
+    where
+        helper :: Tree Int -> Int -> Bool
+        helper tree 0 = True
+        helper tree level =
+            if (levelSum (level - 1) tree) < (levelSum level tree)
+            then helper tree (level - 1)
+            else False
+
 main :: IO ()
 main = do
     print "Hello"
@@ -87,6 +146,18 @@ main = do
     print $ balance 3 [2, 10, 15]     == 2 -- 2 -- Тук трябва да махнем 10 и 15
     print $ balance 1 [5, 10, 15, 36] == 4 -- 4 -- Тук трябва да махнем всички елементи от списъка
 
+    putStrLn "\n\n5zad"
     let haskellRepeat = repeater "I love Haskell"
     print $ haskellRepeat 3 " "    -- "I love Haskell I love Haskell I love Haskell"
     print $ repeater "Quack" 5 "!" --"Quack!Quack!Quack!Quack!Quack"
+
+    putStrLn "\n\n7zad"
+    print $ 0 == levelSum 0 Empty
+    print $ 1 == levelSum 0 (Node 1 (Node 2 Empty Empty) (Node 3 Empty Empty))
+    print $ 5 == levelSum 1 (Node 1 (Node 2 Empty Empty) (Node 3 Empty Empty))
+    print $ 0 == levelSum 2 (Node 1 (Node 2 Empty Empty) (Node 3 Empty Empty))
+    
+    print $ True  == cone Empty
+    print $ True  == cone (Node 1 (Node 2 Empty Empty) (Node 3 Empty Empty))
+    print $ False == cone (Node 5 (Node 2 Empty Empty) (Node 3 Empty Empty))
+    print $ False == cone (Node 1 (Node 2 Empty Empty) (Node 3 Empty (Node 4 Empty Empty)))
